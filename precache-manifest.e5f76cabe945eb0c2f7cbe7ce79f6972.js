6c252af6b2bf41e55ed97b3907335cc0272c254e
Chris Thompson <chris.tx86@gmail.com>
2020-08-21 17:46:59 -0700

    Updates

---
+++ precache-manifest.e5f76cabe945eb0c2f7cbe7ce79f6972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca79e5c6e31cbf155c26c48c1cf3deab",
+    "url": "/React-Expense-Tracker/index.html"
+  },
+  {
+    "revision": "4343953142de93ef988a",
+    "url": "/React-Expense-Tracker/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "a3886eb48eeb822dea54",
+    "url": "/React-Expense-Tracker/static/js/2.abb637fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Expense-Tracker/static/js/2.abb637fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4343953142de93ef988a",
+    "url": "/React-Expense-Tracker/static/js/main.adc60cfc.chunk.js"
+  },
+  {
+    "revision": "65068253a1e1ccfd025d",
+    "url": "/React-Expense-Tracker/static/js/runtime-main.4003a80c.js"
+  }
+]);
